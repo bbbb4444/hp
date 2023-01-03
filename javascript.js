7f97b18bccf2242da04b73801bc4b56010d3e797
bbbb4444 <35387058+bbbb4444@users.noreply.github.com>
2023-01-02 21:07:05 -0500

    Inquiry page -> Submit animation

--- javascript.js
+++ javascript.js
@@ -1,5 +1,43 @@
+
+
+window.addEventListener("load", function() {
+    const form = document.getElementById('inquiry-form');
+    form.addEventListener("submit", function(e) {
+        changeToLoadingGif();
+        e.preventDefault();
+        const data = new FormData(form);
+        const action = e.target.action;
+        fetch(action, {
+            method: 'POST',
+            body: data,
+        })
+            .then(() => {
+                deleteFormElement();
+            })
+    });
+});
+
 function deleteFormElement() {
-    var form = document.getElementById("inquiry-form");
-    document.getElementById("img-container").style.display = "inherit";
+    let form = document.getElementById("inquiry-form");
     form.parentNode.removeChild(form);
+    document.getElementById("img-container").style.display = "inherit";
 }
+
+function changeToLoadingGif() {
+    let submit = document.getElementById("inquiry-form__submit");
+    let spinner = document.getElementById("spinner");
+    let sendText = document.getElementById("submit__text");
+    submit.style.backgroundPosition = "0px -150px";
+    sendText.parentNode.removeChild(sendText);
+
+    submit.style.borderRadius = "100%";
+    submit.style.height = "5.0em";
+    submit.style.width = "5.0em";
+    setTimeout(function () {
+        spinner.style.display = "inherit";
+    }, 20)
+
+
+
+}
+
